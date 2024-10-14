2e321e092cad5b9e2bc9d7d5ed1df28c225ab405
Scorpian <vandaleyindustriesllc@gmail.com>
2024-10-14 14:37:42 -0400

    added comments

--- src/app/pages/Landing.tsx
+++ src/app/pages/Landing.tsx
@@ -4,6 +4,9 @@ export default function Landing() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold mb-6">Calendar QR Code Generator</h1>
+      <p>The final version will look nicer.  This is a prototype/proof of concept.</p>
+      <p>Anything can go here.</p>
+      <p></p>
       <Calendar />
     </div>
   );
